29cc60c98409ee6f5fe3eb4d64627a54b40b3cb4
Domara Shlimon <domara2000@gmail.com>
2021-07-26 15:48:38 -0400

    added options for loading textures

--- AliceEngine/alc/content/texture.cpp
+++ AliceEngine/alc/content/texture.cpp
@@ -8,19 +8,21 @@
 namespace alc {
 
 	texture texture::load(const std::string& filepath) {
-		return load(filepath, format::any);
+		// use default options
+		textureopts options;
+		return load(filepath, options);
 	}
 
-	texture texture::load(const std::string& filepath, format forceformat) {
+	texture texture::load(const std::string& filepath, const textureopts& options) {
 
 		// set format type
 		int tryformat = STBI_default;
 
-		switch (forceformat) {
-			case alc::texture::format::r: tryformat = STBI_grey; break;
-			case alc::texture::format::rg: tryformat = STBI_grey_alpha; break;
-			case alc::texture::format::rgb: tryformat = STBI_rgb; break;
-			case alc::texture::format::rgba: tryformat = STBI_rgb_alpha; break;
+		switch (options.format) {
+			case alc::textureformat::r: tryformat = STBI_grey; break;
+			case alc::textureformat::rg: tryformat = STBI_grey_alpha; break;
+			case alc::textureformat::rgb: tryformat = STBI_rgb; break;
+			case alc::textureformat::rgba: tryformat = STBI_rgb_alpha; break;
 			default: break;
 		}
 
@@ -34,13 +36,13 @@ namespace alc {
 		}
 
 		// get format type
-		format realformat = forceformat;
-		if (forceformat == format::any) {
+		textureformat realformat = options.format;
+		if (realformat == textureformat::any) {
 			switch (channels) {
-				case STBI_grey: realformat = format::r; break;
-				case STBI_grey_alpha: realformat = format::rg; break;
-				case STBI_rgb: realformat = format::rgb; break;
-				case STBI_rgb_alpha: realformat = format::rgba; break;
+				case STBI_grey: realformat = textureformat::r; break;
+				case STBI_grey_alpha: realformat = textureformat::rg; break;
+				case STBI_rgb: realformat = textureformat::rgb; break;
+				case STBI_rgb_alpha: realformat = textureformat::rgba; break;
 				default: break;
 			}
 		}
@@ -48,10 +50,10 @@ namespace alc {
 		// get mode
 		int mode;
 		switch (realformat) {
-			case alc::texture::format::r: mode = GL_RED; break;
-			case alc::texture::format::rg: mode = GL_RG; break;
-			case alc::texture::format::rgb: mode = GL_RGB; break;
-			case alc::texture::format::rgba: mode = GL_RGBA; break;
+			case alc::textureformat::r: mode = GL_RED; break;
+			case alc::textureformat::rg: mode = GL_RG; break;
+			case alc::textureformat::rgb: mode = GL_RGB; break;
+			case alc::textureformat::rgba: mode = GL_RGBA; break;
 			default: break;
 		}
 
@@ -64,11 +66,42 @@ namespace alc {
 		glTexImage2D(GL_TEXTURE_2D, 0, mode, width, height, 0, mode, GL_UNSIGNED_BYTE, pixels);
 
 		// wrapping and filtering options
-		// TODO: rely on arguments to change these
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		switch (options.wrap) {
+			case texturewrap::clamp_edge:
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+				break;
+			case texturewrap::clamp_border:
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+				break;
+			case texturewrap::repeat:
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+				break;
+			case texturewrap::mirrored_repeat:
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+				break;
+			case texturewrap::mirror_clamp_edge:
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRROR_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRROR_CLAMP_TO_EDGE);
+				break;
+			default: break;
+		}
+
+		switch (options.filter) {
+			case texturefilter::linear:
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				break;
+			case texturefilter::nearest:
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				break;
+			default: break;
+		}
 
 		// unbind
 		glBindTexture(GL_TEXTURE_2D, 0);
@@ -79,8 +112,10 @@ namespace alc {
 		// create texture object and return
 		texture tex;
 		tex.m_id = textureID;
-		tex.m_format = realformat;
 		tex.m_size = glm::uvec2(width, height);
+		tex.m_format = realformat;
+		tex.m_wrap = options.wrap;
+		tex.m_filter = options.filter;
 		return tex;
 	}
 
@@ -89,10 +124,12 @@ namespace alc {
 		glDeleteTextures(1, &tex.m_id);
 	}
 
-	texture::texture(std::nullptr_t) : m_id(-1), m_size(0), m_format(format::any) { }
+	texture::texture(std::nullptr_t) 
+		: m_id(-1), m_size(0), m_format(textureformat::any)
+		, m_wrap(texturewrap::clamp_border), m_filter(texturefilter::linear) { }
 
 	bool texture::is_valid() const {
-		return m_format != format::any;
+		return m_format != textureformat::any;
 	}
 
 	texture::operator bool() const { return is_valid(); }
@@ -105,6 +142,18 @@ namespace alc {
 
 	glm::uvec2 texture::get_size() const {
 		return m_size;
+	}
+
+	textureformat texture::get_format() const {
+		return m_format;
+	}
+
+	texturewrap texture::get_wrap() const {
+		return m_wrap;
+	}
+
+	texturefilter texture::get_filter() const {
+		return m_filter;
 	}
 
 	bool texture::operator==(const texture& other) const {
--- AliceEngine/alc/content/texture.hpp
+++ AliceEngine/alc/content/texture.hpp
@@ -4,21 +4,39 @@
 
 namespace alc {
 
-	class texture {
+	enum class textureformat : uint8 {
+		r, rg, rgb, rgba,
+
+		// used to load the default format of a texture 
+		any = static_cast<uint8>(-1)
+	};
+
+	enum class texturewrap : uint8 {
+		clamp_edge,
+		clamp_border,
+		repeat,
+		mirrored_repeat,
+		mirror_clamp_edge
+	};
+
+	enum class texturefilter : uint8 {
+		linear, nearest
+	};
+
+	struct textureopts final {
+		textureformat format = textureformat::any;
+		texturewrap wrap = texturewrap::clamp_edge;
+		texturefilter filter = texturefilter::linear;
+	};
+
+	class texture final {
 	public:
-
-		enum class format : uint8 {
-			r, rg, rgb, rgba,
-
-			// used to load the default format of a texture 
-			any = static_cast<uint8>(-1)
-		};
 
 		// loads a texture 
 		static texture load(const std::string& filepath);
 
 		// loads a texture 
-		static texture load(const std::string& filepath, format forceformat);
+		static texture load(const std::string& filepath, const textureopts& options);
 
 		// deletes a texture
 		static void unload(texture& tex);
@@ -42,6 +60,15 @@ namespace alc {
 		// returns the texture size
 		glm::uvec2 get_size() const;
 
+		// returns the format of the texture
+		textureformat get_format() const;
+		
+		// returns the wrap of the texture
+		texturewrap get_wrap() const;
+		
+		// returns the filter of the texture
+		texturefilter get_filter() const;
+
 		// compare the textures
 		bool operator==(const texture& other) const;
 
@@ -51,7 +78,9 @@ namespace alc {
 	private:
 		uint32 m_id;
 		glm::uvec2 m_size;
-		format m_format;
+		textureformat m_format;
+		texturewrap m_wrap;
+		texturefilter m_filter;
 	};
 
 }
